9dc3049bf76f9f39e0336d280780977112a2c020
Serhiiiiko <2021elit0049@ms.sumdu.edu.ua>
2025-04-29 13:04:39 +0300

    upd2

---
+++ GameStore/Views/Games/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GameStore.Views.Games
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
